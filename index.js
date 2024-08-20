5c213f89e4c17405ffdd52ef61b25f335f61d8b9
Jason Creviston <the@j4w8n.com>
2024-08-20 06:58:52 -0400

    poc

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Hello via Bun!");
+/** @import { XinkConfig } from './types.js' */
+/** @import { BunPlugin } from 'bun' */
+import { CONFIG } from './lib/constants.js'
+import { initRouter } from './lib/utils/router.js'
+
+/** @type {XinkConfig} */
+let config
+
+/**
+ * @type {BunPlugin}
+ */
+const xink_plugin = {
+  name: 'xink-bun-plugin',
+  async setup (build) {
+    const mode = Bun.env['npm_lifecycle_event']
+    console.log('mode:', mode)
+    console.log('config:', config)
+
+    await initRouter(config)
+
+    console.log('end setup')
+  }
+}
+
+/**
+ * @param {XinkConfig} [xink_config]
+ * @returns {BunPlugin}
+ */
+export function xink(xink_config = CONFIG) {
+  console.log('init xink plugin')
+
+  // TODO - validate and merge with default config
+  config = xink_config
+
+  return xink_plugin
+}
---
+++ types.d.ts
@@ -0,0 +1,29 @@
+import type { BunPlugin } from "bun"
+import type { Plugin } from "vite"
+import type { Router } from "./lib/utils/medley.js"
+
+export type Handle = (event: RequestEvent, resolve: ResolveEvent) => MaybePromise<Response>
+export type MaybePromise<T> = T | Promise<T>
+export type RequestEvent = {
+  headers: Omit<Headers, 'toJSON' | 'count' | 'getAll'>;
+  locals: { [key: string]: string },
+  params: Params;
+  request: Request;
+  route: Route;
+  url: Omit<URL, 'createObjectURL' | 'revokeObjectURL' | 'canParse'>;
+}
+export type ResolveEvent = (event: RequestEvent) => MaybePromise<Response>
+export type XinkConfig = {
+  params?: string;
+  routes?: string;
+}
+
+export async function xink(xink_config?: XinkConfig): BunPlugin
+export function json(data: any, init?: ResponseInit | undefined): Response
+export function text(data: string, init?: ResponseInit | undefined): Response
+export function sequence(...handlers: Handle[]): Handle
+export class Xink {
+  constructor() {}
+  async fetch(request: Request): Promise<Response>
+}
+export const router: Router
