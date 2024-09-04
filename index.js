c9bed22ab9f856b5c3fdd4cb9e628a95bca726e1
Jason Creviston <the@j4w8n.com>
2024-09-04 07:40:08 -0400

    fix: paths

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ const xink_plugin = {
   name: 'xink-bun-plugin',
   async setup (build) {
     console.log('config', config)
+    console.log('build config', build.config)
+    const mode = Bun.env.npm_lifecycle_event
 
-    if (build.config) {
+    if (mode === 'build' && build.config) {
       console.log('build', build)
       await initRouter(config, false, build.config.outdir)
-    } else {
+    } else if (mode === 'dev') {
       console.log('deving...')
       await initRouter(config, true)
     }
