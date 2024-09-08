3340336496288495c77188e38b98ea9291d0abac
Jason Creviston <the@j4w8n.com>
2024-09-08 18:43:36 -0400

    fix: type

--- types.d.ts
+++ types.d.ts
@@ -19,7 +19,7 @@ export type XinkConfig = {
   routes?: string;
 }
 
-export async function xink(xink_config?: XinkConfig): BunPlugin
+export function xink(xink_config?: XinkConfig): BunPlugin
 export function html(data: any, init?: ResponseInit | undefined): Response
 export function json(data: any, init?: ResponseInit | undefined): Response
 export function text(data: string, init?: ResponseInit | undefined): Response
