43ed66743c98fd9c08e5035e2dca3837857808b7
Jason Creviston <the@j4w8n.com>
2024-09-09 09:48:05 -0400

    checkpoint

--- types.d.ts
+++ types.d.ts
@@ -1,10 +1,17 @@
 import type { BunPlugin } from "bun"
 import type { Router } from "./lib/router.js"
 import type { Params, Route } from "./lib/types/internal.js"
+import type { CookieSerializeOptions } from "cookie"
 
+export type Cookies = {
+  get(name: string, options?: CookieSerializeOptions): string | undefined;
+  getAll(options?: CookieSerializeOptions): Array<{ name: string, value: string }>;
+  set(name: string, value: string, options: CookieSerializeOptions & { path?: string }): void;
+}
 export type Handle = (event: RequestEvent, resolve: ResolveEvent) => MaybePromise<Response>
 export type MaybePromise<T> = T | Promise<T>
 export type RequestEvent = {
+  cookies: Cookies;
   headers: Omit<Headers, 'toJSON' | 'count' | 'getAll'>;
   locals: { [key: string]: string },
   params: Params;
